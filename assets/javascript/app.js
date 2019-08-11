146487ffdc823f5de303860fd0ea68e126853865
Irvin Irra <irv.irra@gmail.com>
2019-08-11 15:42:03 -0600

    added functionality to be able to search Gifs through api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,20 +2,31 @@
 var queryData;
 var queryLimit = 10;
 
-searchGiphy ('test')
-function searchGiphy(query) {
+
+function searchGiphy()  {
+    event.preventDefault(); // prevent form submit from refreshing page
     
-    // var limit = queryLimit;
-    // var q = query;
-    // var api_key = "api_key=Z4AgULFMGctT1JzJ14D7hWtktVj9HaWq";
-    // var searchQuery = "https://api.giphy.com/v1/gifs/search?";
-    // var offset = "0";
-    // var rating = "G";
-    // var lang = "en";
+
+    var api_key = "Z4AgULFMGctT1JzJ14D7hWtktVj9HaWq";
+    var searchQuery = "https://api.giphy.com/v1/gifs/search?";
+    var offset = "0";
+    var rating = "G";
+    var lang = "en";
+    var query = $('#queryInput').val();
 
 
+    searchQuery += "api_key="+ api_key;
+    searchQuery += "&q=" + query;
+    searchQuery += "&limit=" + queryLimit;
+    searchQuery += "&offset=" + offset;
+    searchQuery += "&rating=" + rating;
+    searchQuery += "&lang=" + lang;
 
-    var xhr = $.get("https://api.giphy.com/v1/gifs/search?api_key=Z4AgULFMGctT1JzJ14D7hWtktVj9HaWq&q=dog&limit=10&offset=0&rating=G&lang=en");
+    // var xhr = $.get("https://api.giphy.com/v1/gifs/search?api_key=Z4AgULFMGctT1JzJ14D7hWtktVj9HaWq&q=dog&limit=10&offset=0&rating=G&lang=en");
+    
+    console.log (searchQuery);
+    var xhr = $.get(searchQuery);
+
     xhr.done(function (data) {
         console.log("success got data", data);
         queryData = data;
